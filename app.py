989bff7f608e4e2da08a2709cba5c2ea244af084
Saniyaanjum23 <shaiksaniya2305@gmail.com>
2026-08-04 10:49:02 +0000

    updated

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,jsonify,request
 
 app=Flask(__name__)
 
@@ -26,9 +26,16 @@ def login():
 def trainers():
     return render_template("trainers.html")
  
-@app.route("/register")
+@app.route('/register',methods=["POST","GET"])
 def register():
+    if request.method=="POST":
+        name=request.form["name"]
+        email=request.form["email"]
+        password=request.form["password"]
+        dob=request.form["dob"]
+        gender=request.form["gender"]
+        course=request.form["course"]
     return render_template("register.html")
-
+    
 if __name__=='__main__':
     app.run (debug=True)
